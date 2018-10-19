cbb2ba519d18e574e3d2c24b7dc30709bc6e0a12
favour-umoru <43656344+favour-umoru@users.noreply.github.com>
2018-10-19 13:19:32 -0500

    Update functions.cpp

--- functions.cpp
+++ functions.cpp
@@ -6,12 +6,10 @@ using namespace std;
 int colorPath(const vector<vector<int>>& heightMap, vector<vector<int>>& r, vector<vector<int>>& g, vector<vector<int>>& b, int color_r, int color_g,
 	int color_b, int start_row)
 {
-	int dist = 0;
-	
 	int elevChange1 = 99999;
 	int elevChange2 = 99999;
 	int elevChange3 = 99999;
-	int totalElevChange = 99999;
+	int totalElevChange = 0;
 	
 	// Go through the elevation data
 	for(int i = 0; i < heightMap.size(); i++)
@@ -40,43 +38,32 @@ int colorPath(const vector<vector<int>>& heightMap, vector<vector<int>>& r, vect
 			// Find the Minimum change in elevation by comparing the elevation changes 
 			int minElevationChange = heightMap.at(start_row).at(j));
 			
-			if((elevChange1 < elevChange2)
+			if((elevChange1 < elevChange2 && elevChange1 < elevChange3)
 			{
 				minElevationChange = elevChange1;
-				
-				if(elevChange1 < elevChange3)
-				{
-					minElevationChange = elevChange1;
-					R.at(start_row + i + 1).at(j - 1) = 252;
-					G.at(start_row + i + 1).at(j - 1) = 25;
-					B.at(start_row + i + 1).at(j - 1) = 63;
-				}
+				minElevationChange = elevChange1;
+				R.at(start_row + i + 1).at(j - 1) = 252;
+				G.at(start_row + i + 1).at(j - 1) = 25;
+				B.at(start_row + i + 1).at(j - 1) = 63;
 				
 			}
-			else if((elevChange2 < elevChange1)
+			else if((elevChange2 < elevChange1 && elevChange2 < elevChange3)
 			{
 				minElevationChange = elevChange2;
-				
-				if(elevChange2 < elevChange3)
-				{
-					minElevationChange = elevChange2;
-					R.at(start_row + i + 1).at(j) = 252;
-					G.at(start_row + i + 1).at(j) = 25;
-					B.at(start_row + i + 1).at(j) = 63;
-				}
+				minElevationChange = elevChange2;
+				R.at(start_row + i + 1).at(j) = 252;
+				G.at(start_row + i + 1).at(j) = 25;
+				B.at(start_row + i + 1).at(j) = 63;
 				
 			}
-			else if((elevChange3 < elevChange1)
+			else if((elevChange3 < elevChange1 && elevChange3 < elevChange2)
 			{
 				minElevationChange = elevChange3;
-				
-				if(elevChange3 < elevChange2)
-				{
-					minElevationChange = elevChange3;
-					R.at(start_row + i + 1).at(j + 1) = 252;
-					G.at(start_row + i + 1).at(j + 1) = 25;
-					B.at(start_row + i + 1).at(j + 1) = 63;
-				}
+					
+				minElevationChange = elevChange3;
+				R.at(start_row + i + 1).at(j + 1) = 252;
+				G.at(start_row + i + 1).at(j + 1) = 25;
+				B.at(start_row + i + 1).at(j + 1) = 63;
 				
 			}
 				
